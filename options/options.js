610ba68e8b4c9e262fb6103e61ac1f03dc2f1c2f
Crowdbotics <team@crowdbotics.com>
2024-03-06 21:15:22 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "procompounding-46847",
+  url: "https://procompounding-46847.botics.co",
+  api: "https://procompounding-46847.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
